872207b445d6d23f880a46941aad14bd930d9bb2
Kateryna <katerynabatrakova@gmail.com>
2020-08-19 23:32:13 +0000

    Populate admin dashboard table

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   # Use the basic HTTP authentication to secure the whole application from non-authorised users
   http_basic_authenticate_with name: ENV["name"], password: ENV["password"]
   def show
+
+
+
+    
   end
 end
