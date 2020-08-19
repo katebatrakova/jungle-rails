68df937a9b3f65c2abf4ec578623454b38bfc3d6
Kateryna <katerynabatrakova@gmail.com>
2020-08-19 22:51:07 +0000

    HTTP authentication for admin's pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  # Use the basic HTTP authentication to secure the whole application from non-authorised users
+  http_basic_authenticate_with name: ENV["name"], password: ENV["password"]
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  # Use the basic HTTP authentication to secure the whole application from non-authorised users
+  http_basic_authenticate_with name: ENV["name"], password: ENV["password"]
   def index
     @products = Product.order(id: :desc).all
   end
