7e16e9c78ff8813f763fc5deb5ace39dc0cac79d
Kateryna <katerynabatrakova@gmail.com>
2020-08-25 19:43:27 +0000

    Create simple home page feature spec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.secret_token = SecureRandom.hex(64)
+
+
+
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
